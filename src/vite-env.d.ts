0777d1b2a3c59b3f0543db006683d599c2b9fd1d
JZims <zimmerman.j@hotmail.com>
2025-02-17 16:42:00 -0500

    edits to csv export file

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,3 @@
 /// <reference types="vite/client" />
+VITE_CSV_PATH = '/Users/jzimms/Development/Veda/Cocktail Creator/cocktail-creator-frontend/src/assets/scripts/data/veda_cocktails_cleaned.csv'
+VITE_OUTPUT_PATH = '/Users/jzimms/Development/Veda/Cocktail Creator/cocktail-creator-frontend/src/assets/scripts/output/veda_cocktails.json'
