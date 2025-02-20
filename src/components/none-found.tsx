1ff4a3b21e6bd475b6f29e124df7bf41b9119a4e
JZims <zimmerman.j@hotmail.com>
2025-02-20 02:21:18 -0500

    Added a line for selection clarification

--- src/components/none-found.tsx
+++ src/components/none-found.tsx
@@ -4,6 +4,8 @@ export function NoneFound() {
     <div className="none-found">
       <h2>No drinks found</h2>
       <h3>Try a different combination!</h3>
+      <p> <strong>Tip: Select multiple options in each category</strong></p>
+      
     </div>
   );
 }
